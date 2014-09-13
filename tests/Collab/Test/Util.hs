047dc1d7b1fd55de087f4a360e111ed8ff889c32
Dennis Dietrich <d.dietrich84@gmail.com>
2014-09-13 10:29:31 +0200

    cleanup

--- tests/Collab/Test/Util.hs
+++ tests/Collab/Test/Util.hs
@@ -1,29 +1,11 @@
 module Collab.Test.Util
   ( runServerApp
-  , withClient
-  , withNoopClient
   ) where
 
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad (forever, when, guard)
-import Control.Exception (bracket)
-import Control.Applicative ((<$>))
-import Control.Concurrent (forkIO, newMVar, killThread)
-import Data.Text (Text)
+import Control.Concurrent (newMVar)
 import Network.WebSockets
-import Test.Hspec
-import Collab.App (app, parseMessage)
-import Collab.Json
-import Collab.Util (textToByteString)
-import Data.Aeson (decode)
-import Data.Maybe (fromJust)
-import Control.Concurrent.MVar
+import Collab.App (app)
 
 runServerApp = do
   state <- newMVar []
   runServer "127.0.0.1" 9000 $ app state
-
-withClient :: ClientApp () -> IO ()
-withClient = runClient "localhost" 9000 "/foo"
-
-withNoopClient a = withClient $ \_ -> a
