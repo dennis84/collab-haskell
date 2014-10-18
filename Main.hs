114242a331adcd8e9ae8ff3bce7612545c443933
Dennis Dietrich <d.dietrich84@gmail.com>
2014-10-18 14:21:59 +0200

    works

--- Main.hs
+++ Main.hs
@@ -1,17 +1,19 @@
 import Control.Applicative ((<$>))
-import Control.Concurrent (newMVar)
-import Control.Exception
 import Network.WebSockets (runServer)
-import System.Environment (getEnv)
+import System.Environment (getArgs)
 import Collab.App (app)
 import qualified Collab.State as State
 
-getPort :: Int -> IO Int
-getPort d = (read <$> getEnv "PORT") `catch` \(SomeException _) -> return d
+conf :: IO (String, Int)
+conf = parse <$> getArgs
+  where local = ("127.0.0.1", 9000)
+        parse []            = local
+        parse (host:[])     = local
+        parse (host:port:_) = (host, read port)
 
 main :: IO ()
 main = do
-  port  <- getPort 9000
+  (host, port) <- conf
   state <- State.new
-  putStrLn $ "Listening on port " ++ show port
-  runServer "127.0.0.1" port $ app state
+  putStrLn $ "Listening on " ++ host ++ ":" ++ show port
+  runServer host port $ app state
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: cabal run
+web: cabal run "0.0.0.0" $PORT
